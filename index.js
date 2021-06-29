57c877b036ce5d3810e1ca47db4a6f37e2590864
fbaud <fbaud@p2pventure.org>
2021-06-29 09:35:02 +0200

    version 0.30.10

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * @author P2PMoney.org
+ * @name @p2pmoney-org/ethereum_core
  * @homepage http://www.p2pmoney.org/
  * @license MIT
  */
@@ -14,12 +15,26 @@ if ( typeof window !== 'undefined' && window  && (typeof window.simplestore === 
 
 	window.simplestore = {};
 	
-	window.simplestore.nocreation = true;
+	window.simplestore.nocreation = true; // tell Bootstrap object we created simplestore
 	
 } else if ((typeof global !== 'undefined') && (typeof global.simplestore === 'undefined')) {
 	// nodejs
 	console.log('creating global.simplestore in @p2pmoney-org/ethereum_core index.js');
 	global.simplestore = {};
+}
+
+// could be mutualized with above, but quick implementation for version 0.30.10 to avoid regressions
+var globalscope;
+
+if ( typeof window !== 'undefined' && window ) {
+	globalscope = window;
+}
+else if (typeof global !== 'undefined') {
+	globalscope = global;
+}
+
+if (globalscope.simplestore) {
+	globalscope.simplestore.noconsoleoverload = true; // tell Bootstrap object not to overload console log
 }
 
 const Ethereum_core = require('./ethereum_core.js');
